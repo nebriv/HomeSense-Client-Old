960c9637cc97bf920d0ba2299fa1536c4f04c4ba
nebriv <ben@benvirgilio.com>
2018-02-18 17:55:50 -0500

    Display Updates

--- display.py
+++ display.py
@@ -25,18 +25,18 @@ class Display:
         # Move left to right keeping track of the current x position for drawing shapes.
         x = padding
         # Draw an ellipse.
-        draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
-        x += shape_width+padding
+        #draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
+        #x += shape_width+padding
         # Draw a rectangle.
-        draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
-        x += shape_width+padding
+        #draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
+        #x += shape_width+padding
         # Draw a triangle.
-        draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
-        x += shape_width+padding
+        #draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
+        #x += shape_width+padding
         # Draw an X.
-        draw.line((x, bottom, x+shape_width, top), fill=255)
-        draw.line((x, top, x+shape_width, bottom), fill=255)
-        x += shape_width+padding
+        #draw.line((x, bottom, x+shape_width, top), fill=255)
+        #draw.line((x, top, x+shape_width, bottom), fill=255)
+        #x += shape_width+padding
         # Load default font.
         font = ImageFont.load_default()
         # Write two lines of text.
