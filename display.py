ab33b4f6953252b42476d45c5667cea74d5f0814
nebriv <ben@benvirgilio.com>
2019-11-05 13:47:43 -0500

    Logger updates

--- display.py
+++ display.py
@@ -10,6 +10,16 @@ from textwrap import wrap
 import logging
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create the logging file handler
+fh = logging.FileHandler("homesense.log")
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+# add handler to logger object
+logger.addHandler(fh)
 
 class Display:
     RST = 24
